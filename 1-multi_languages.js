788bf3770f9f31fadf3d1b8c98d911caaafbd0b7
Mary Magdalene Achero <marymagdaleneachero@gmail.com>
2025-07-11 13:00:01 +0300

    Updated content in the file

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
 let firstLine = "C is fun";
 let secondLine = "Python is cool";
-let thirdLine = "Javscript is amazing";
+let thirdLine = "JavaScript is amazing";
 console.log(firstLine);
 console.log(secondLine);
 console.log(thirdLine);
